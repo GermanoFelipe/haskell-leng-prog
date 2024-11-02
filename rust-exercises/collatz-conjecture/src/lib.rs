5d6a51efb49c0e524909d72246b9cadb321f4b07
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-02 13:38:17 -0300

    fractions and collatz

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,15 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+    result_collatz(n, 0)
+}
+
+pub fn result_collatz(mut n: u64, mut counter: u64) -> Option<u64> {
+    if n <= 0 {
+        return None;
+    }
+    if n == 1 {
+        return Some(counter)
+    }
+    let num = if n % 2 == 0 {n / 2} else { n*3 + 1 };
+    result_collatz(num, counter + 1)
 }
 
--- rust-exercises/fraction/src/lib.rs
+++ rust-exercises/fraction/src/lib.rs
@@ -2,30 +2,40 @@ type Fraction = (i32, i32);
 
 /// Add 2 fractions
 pub fn add((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    if d1 == 0 || d2 == 0{
+        panic!("Cannot divide by zero");
+    }
+    if d1 == d2{
+        return (n1 + n2, d1) as Fraction;
+    }
+    return (n1 * d2 + n2 * d1, d1 * d2) as Fraction;
 }
 
 /// Subtract 2 fractions
 pub fn sub((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    return add((n1,d1), ((-1) * n2, d2));
 }
 
 /// Multiply 2 fractions
 pub fn mul((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    return (n1 * n2, d1 * d2) as Fraction;
 }
 
 /// Divide 2 fractions
 pub fn divide((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    return mul((n1,d1), (d2,n2));
 }
 
 /// Calculate the Highest common factor between 2 numbers
 pub fn hcf(a: i32, b: i32) -> i32 {
-    panic!("Implement it");
+    if b == 0{
+        return a;
+    }
+    return hcf(b, a % b);
 }
 
 /// Create a fraction simplifying with the arguments simplified by the `hcf`
 pub fn simplify(n: i32, d: i32) -> Fraction {
-     (n, d)
+    let m = hcf(n,d);
+    return (n/m, d/m) as Fraction;
 }

cd2323248dc1543c619c49e7fbb7e82a4136a010
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-02 15:47:42 -0300

    fraction type

--- rust-exercises/fraction_type/src/lib.rs
+++ rust-exercises/fraction_type/src/lib.rs
@@ -1,15 +1,50 @@
+use std::arch::x86_64::_mm256_andnot_pd;
 use std::ops;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Fraction(pub i32, pub i32);
 
 impl Fraction {
-    
+    pub fn add(&self, other: Fraction) -> Fraction {
+        let (n1, d1) = (self.0, self.1);
+        let (n2, d2) = (other.0, other.1);
+        if d1 == 0 || d2 == 0 {
+            panic!("Cannot divide by zero")
+        }
+        let (n, d) = (n1 * d2 + n2 * d1, d1 * d2);
+        simplify(n, d)
+    }
+
+    pub fn sub(&self, other: Fraction) -> Fraction {
+        let (n1, d1) = (self.0, self.1);
+        let (n2, d2) = (other.0, other.1);
+        if d1 == 0 || d2 == 0 {
+            panic!("Cannot divide by zero")
+        }
+        let (n, d) = (n1 * d2 - n2 * d1, d1 * d2);
+        simplify(n, d)
+    }
+
+    pub fn mul(&self, other: Fraction) -> Fraction {
+        let (n1, d1) = (self.0, self.1);
+        let (n2, d2) = (other.0, other.1);
+        let (n, d) = (n1 * n2, d1 * d2);
+        simplify(n, d)
+    }
+
+    pub fn divide(&self, other: Fraction) -> Fraction {
+        let (n1, d1) = (self.0, self.1);
+        let (n2, d2) = (other.0, other.1);
+        let (n, d) = (n1 * d2, n2 * d1);
+        simplify(n, d)
+    }
 }
 
 impl ops::Add for Fraction {
     type Output = Fraction;
-
+    fn add(self, other: Fraction) -> Self::Output {
+        Fraction::add(&self, other)
+    }
     
 }
 

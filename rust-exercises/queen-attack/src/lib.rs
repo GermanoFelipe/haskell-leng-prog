9e54787988d5cdf320852ffe550bfe5ef4b0a1df
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-02 16:43:41 -0300

    queen

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,23 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        if rank < 0 || file < 0 || rank >= 8 || file >= 8 {return None};
+        Some(ChessPosition(rank, file))
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
-}
+        if self.position.0 == other.position.0 ||
+            self.position.1 == other.position.1 ||
+            (self.position.1 - other.position.1).abs() ==
+            (self.position.0 - other.position.0).abs() {
+            true
+        }
+        else { false }
+    }
 }

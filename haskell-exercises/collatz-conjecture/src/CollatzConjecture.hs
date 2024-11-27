5c3a9b7ae2e5d023edad09058a9317b1cfb75d8f
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-27 15:43:15 -0300

    collatz again

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,8 +1,12 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n
-  | n <= 0 = Nothing
-  | n == 1 = Just 0
-  | n `mod` 2 == 0 = fmap (+1) $ (collatz (n `div` 2))
-  | otherwise = fmap(+1) $ collatz (3 * n + 1)
+collatz n = recursiveCollatz n 0
+
+recursiveCollatz :: Integer -> Integer -> Maybe Integer
+recursiveCollatz n c
+    | n <= 0 = Nothing
+    | n == 1 = Just c
+    | otherwise = recursiveCollatz number (c + 1)
+    where
+        number = if (n `mod` 2) == 0 then (n `div` 2) else (3 * n + 1)
